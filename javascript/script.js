4e5a68e124187798661b02687bad6ad01d31c173
Vimmi05 <vimmituli1999@gmail.com>
2023-07-03 10:50:29 +0530

    third july

--- javascript/script.js
+++ javascript/script.js
@@ -54,5 +54,9 @@ var swiper = new Swiper(".mySwiper", {
     el: ".swiper-pagination",
     clickable: true,
   },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 });
 
